44b392dde8e220935f210612f7b3d6fc54255f4e
Sean Slattery <sean.slattery203@gmail.com>
2019-12-01 21:41:15 -0800

    day I part II done, implemented using a stack

--- Day_01/tyranny.go
+++ Day_01/tyranny.go
@@ -9,8 +9,44 @@ import (
 	"strings"
 )
 
+type mass struct {
+	value int
+	next  *mass
+}
+
+type Stack struct {
+	size int
+	top  *mass
+}
+
+func (stack *Stack) Len() int {
+	return stack.size
+}
+
+func (stack *Stack) Push(value int) {
+	stack.top = &mass{
+		value: value,
+		next:  stack.top,
+	}
+	stack.size++
+}
+
+func (stack *Stack) Pop() (value int) {
+	if stack.Len() > 0 {
+		value = stack.top.value
+		stack.top = stack.top.next
+		stack.size--
+		return
+	}
+	return 0
+}
+
 func fuelRequired(mass int) int {
-	return (mass / 3) - 2
+	fuel := (mass / 3) - 2
+	if fuel <= 0 {
+		fuel = 0
+	}
+	return fuel
 }
 
 //Takes a newline seperated list of masses and returns total output
@@ -22,8 +58,38 @@ func totalFuelRequired(masses []int) int {
 	return totalFuel
 }
 
+func totalFuelRequiredPartII(masses []int) int {
+	var totalFuel int
+	stack := new(Stack)
+	for _, mass := range masses {
+		stack.Push(mass)
+	}
+
+	for {
+		//fmt.Printf("Stack size: %d\n", stack.Len())
+		//fmt.Printf("Total Fuel: %d\n", totalFuel)
+		if stack.Len() > 0 {
+			mass := stack.Pop()
+			//	fmt.Printf("Mass: %d\n", mass)
+			fuel := fuelRequired(mass)
+			//	fmt.Printf("Fuel: %d\n", fuel)
+			if fuel != 0 {
+				stack.Push(fuel)
+			}
+			totalFuel += fuel
+		} else {
+			//	fmt.Println("Loop should end")
+			break
+		}
+	}
+	return totalFuel
+}
+
 func readMassesFromFile(fn string) ([]int, error) {
 	file, err := os.Open(fn)
+	if err != nil {
+		fmt.Printf("Error opening file %q", err)
+	}
 	defer file.Close()
 
 	if err != nil {
@@ -38,7 +104,6 @@ func readMassesFromFile(fn string) ([]int, error) {
 		if err != nil {
 			break
 		}
-		fmt.Println(line)
 		sanitizedLine := strings.TrimSuffix(line, "\n")
 		i, err := strconv.Atoi(sanitizedLine)
 		if err != nil {
@@ -56,7 +121,9 @@ func readMassesFromFile(fn string) ([]int, error) {
 }
 
 func main() {
-	masses, _ := readMassesFromFile("/Users/seanslattery/dev/AoC_2019/Day_01/test.txt")
+	masses, _ := readMassesFromFile("/Users/seanslattery/dev/AoC_2019/Day_01/masses.txt")
 	total := totalFuelRequired(masses)
 	fmt.Printf("The total amount of fuel required for part I is: %d\n", total)
+	total2 := totalFuelRequiredPartII(masses)
+	fmt.Printf("The total amount of fuel required for part II is: %d\n", total2)
 }
--- Day_01/tyranny_test.go
+++ Day_01/tyranny_test.go
@@ -55,9 +55,21 @@ func TestTotalFuelRequiredPart2(t *testing.T) {
 			t.Errorf("got %d want %d", got, want)
 		}
 	}
-	t.Run("Multiple Mass Part II: 12, 14", func(t *testing.T) {
+	t.Run("Fuel Remainder Single Mass Part II: 654", func(t *testing.T) {
+		masses := []int{1969}
+		got := totalFuelRequiredPartII(masses)
+		want := 966
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("Fuel Remainder Single Mass Part II: 100756", func(t *testing.T) {
+		masses := []int{100756}
+		got := totalFuelRequiredPartII(masses)
+		want := 50346
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("No Fuel Remainder Multiple Mass Part II: 12, 14", func(t *testing.T) {
 		masses := []int{12, 14}
-		got := totalFuelRequiredPart2(masses)
+		got := totalFuelRequiredPartII(masses)
 		want := 4
 		assertCorrectMessage(t, got, want)
 	})
